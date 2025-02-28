2a58c54b675f8efd0fa4dfec346a912e4f1d186b
Сергей Шамбир <sergei.shambir@travelline.ru>
2025-02-28 17:23:05 +0300

    Доработал функцию для подключения xhprof

--- public/index.php
+++ public/index.php
@@ -6,27 +6,31 @@ use App\Controller\WikiBackendAppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 if (extension_loaded('xhprof')) {
-    require_once __DIR__ . '/../src/xhprof/xhprof_lib/utils/xhprof_lib.php';
-    require_once __DIR__ . '/../src/xhprof/xhprof_lib/utils/xhprof_runs.php';
-
     function do_xhprof_profile()
     {
         $minDurationToProfile = 0.1;
         $startedAt = microtime(true);
-        xhprof_enable(XHPROF_FLAGS_NO_BUILTINS | XHPROF_FLAGS_CPU);
+        xhprof_enable(XHPROF_FLAGS_CPU | XHPROF_FLAGS_NO_BUILTINS);
         try {
             register_shutdown_function(function () use ($startedAt, $minDurationToProfile) {
                 register_shutdown_function(function () use ($startedAt, $minDurationToProfile) {
                     try {
-                        $data = xhprof_disable();
+                        $xhprofData = xhprof_disable();
                         $duration = microtime(true) - $startedAt;
-                        if ($duration > $minDurationToProfile) {
-                            $requestUrl = $_SERVER['REQUEST_URI'] ?? '/';
-                            $prefix = trim(preg_replace('/[^a-zA-Z0-9]+/', '-', $requestUrl), '-')
-                                . '-' . sprintf('%.2f', $duration) . 's';
-                            $runs = new XHProfRuns_Default();
-                            $runs->save_run($data, $prefix);
+                        if ($duration < $minDurationToProfile) {
+                            return;
                         }
+                        $requestUrl = $_SERVER['REQUEST_URI'] ?? '/';
+                        if (str_contains($requestUrl, 'autodiscover')) {
+                            return;
+                        }
+                        $prefix = trim(preg_replace('/[^a-zA-Z0-9]+/', '-', $requestUrl), '-')
+                            . '-' . str_replace('.', '-', sprintf('%.2f', $duration)) . 's';
+
+                        require_once __DIR__ . '/../src/xhprof/xhprof_lib/utils/xhprof_lib.php';
+                        require_once __DIR__ . '/../src/xhprof/xhprof_lib/utils/xhprof_runs.php';
+                        $runs = new XHProfRuns_Default();
+                        $runs->save_run($xhprofData, $prefix);
                     } catch (Throwable $e) {
                     }
                 });
